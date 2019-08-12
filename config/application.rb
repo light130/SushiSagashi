1bd22cd66c9e17c3cc16f9a8e3b87a01d320c18e
light130 <light13silver@gmail.com>
2019-08-12 18:04:39 +0900

    Add Japanese version to devise

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.i18n.default_locale = :ja
 
     config.generators do |generator|
       generator.test_frameword = "rspec",
