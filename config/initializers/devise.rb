f09876314042e59de099865986a6aa985cd7670c
light130 <light13silver@gmail.com>
2019-08-21 08:55:49 +0900

    Modified mailer-sender in production environment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  # config.mailer_sender = 'light13silver@gmail.com'
+  if Rails.env.development?
+    config.mailer_sender = 'light13silver@gmail.com'
+  end
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
