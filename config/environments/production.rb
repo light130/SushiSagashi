05339975d770ca8891e86fc741d500107eb5cc63
light130 <light13silver@gmail.com>
2019-08-21 17:06:35 +0900

    Modified mailer-settings for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,11 +69,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'glacial-lowlands-91237.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.default_url_options = { host: 'glacial-lowlands-91237.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
