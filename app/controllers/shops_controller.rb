5b2d6a03c4d8864dec3c3e58fc95702693cacfd4
light130 <light13silver@gmail.com>
2019-08-24 22:47:35 +0900

    Make CRUD about shop

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,9 +1,27 @@
 class ShopsController < ApplicationController
   before_action :authenticate_user!
-  before_action :correct_user, only: [:destroy]
+  before_action :admin_user, only: [:new, :edit, :create, :update, :destroy]
 
   def new
     @shop = current_user.shops.build
+  end
+
+  def edit
+    @shop = Shop.find(params[:id])
+  end
+
+  def update
+    @shop = current_user.shops.find(params[:id])
+    if @shop.update_attributes(shop_params)
+      flash[:notice] = "情報を更新しました。"
+      redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @shop = Shop.find(params[:id])
   end
 
   def create
@@ -16,19 +34,19 @@ class ShopsController < ApplicationController
   end
 
   def destroy
-    @shop.destroy
-    redirect_back(fallback_location: root_url)
+    Shop.find(params[:id]).destroy
+    flash[:notice] = "削除しました。"
+    redirect_to root_url
   end
 
   private
 
-  def correct_user
-    @shop = current_user.shops.find_by(id: params[:id])
-    redirect_to root_url if @shop.nil?
-   end
-
    def shop_params
      params.require(:shop).permit(:name, :address, :budget, :opening_hours, :picture)
+   end
+
+   def admin_user
+     redirect_to root_url unless current_user.admin?
    end
 
 end
