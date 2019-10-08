4737bd43affadce4138f661e7b7c2b49a8edb160
light130 <light13silver@gmail.com>
2019-10-08 20:40:33 +0900

    Update gemfile.lock for heroku

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,17 +1,23 @@
 class GoodsController < ApplicationController
 
   def create
-    shop = Shop.find(params[:shop_id])
+    @shop = Shop.find(params[:shop_id])
     comment = Comment.find(params[:comment_id])
     current_user.good(comment)
-    redirect_back(fallback_location: shop_url(shop))
+    respond_to do |format|
+      format.html { redirect_to @shop }
+      format.js
+    end
   end
 
   def destroy
-    shop = Shop.find(params[:shop_id])
+    @shop = Shop.find(params[:shop_id])
     comment = Comment.find(params[:comment_id])
     current_user.normal(comment)
-    redirect_back(fallback_location: shop_url(shop))
+    respond_to do |format|
+      format.html { redirect_to @shop }
+      format.js
+    end
   end
 
 end
