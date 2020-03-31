33eb8f2c0beb18177e600c964a828ba2c0947ad2
light130 <light13silver@gmail.com>
2020-03-31 23:36:04 +0900

    ajax

--- app/javascript/packs/shop_favorites.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('turbolinks:load', function() {
-  document.querySelectorAll('#like').forEach(function(a) {
-    a.addEventListener('ajax:success', function() {
-      var like_form = a.parentNode
-      like_form.style.display = 'none';
-    });
-  });
-});
